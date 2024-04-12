453a4dbe0baa866a163e1ec8cec694b6047ee648
Kipngetich222 <123062517+Kipngetich222@users.noreply.github.com>
2024-04-12 23:33:25 +0300

    promises

--- async/promise.js
+++ async/promise.js
@@ -42,3 +42,18 @@ const fetchPromise1 = fetch(
       
       fetchProducts();
       
+
+
+
+      //-------------------------------------------------------
+      function alarm(person, delay) {
+        return new Promise((resolve, reject) => {
+          if (delay < 0) {
+            throw new Error("Alarm delay must not be negative");
+          }
+          setTimeout(() => {
+            resolve(`Wake up, ${person}!`);
+          }, delay);
+        });
+      }
+      
